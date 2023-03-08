daf8923c0692948a49c2e64c71720cb29c6f3bfc
Daniel Gabriel <reelpscanal@gmail.com>
2023-03-08 18:50:55 -0300

    commit seo

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -2,6 +2,7 @@ import { queryClient } from '@/lib/react-query'
 import { globalStyles } from '@/styles/global'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { SessionProvider } from 'next-auth/react'
+import { DefaultSeo } from 'next-seo'
 import type { AppProps } from 'next/app'
 
 globalStyles()
@@ -13,6 +14,15 @@ export default function App({
   return (
     <QueryClientProvider client={queryClient}>
       <SessionProvider session={session}>
+        <DefaultSeo
+          openGraph={{
+            type: 'website',
+            locale: 'en_US',
+            url: 'https://www.calendargo.vercel.app/',
+            site_name: 'CalendarGo',
+          }}
+        />
+
         <Component {...pageProps} />
       </SessionProvider>
     </QueryClientProvider>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,22 +4,36 @@ import { Container, Hero, Preview } from './styles'
 import previewImage from '../../assets/calendar.png'
 import Image from 'next/image'
 import { ClaimUsernameForm } from './components/ClaimUsernameForm'
+import { NextSeo } from 'next-seo'
 
 export default function Home() {
   return (
-    <Container>
-      <Hero>
-        <Heading size="4xl">Agendamento descomplicado</Heading>
-        <Text size="xl">
-          Conecte seu calendário e permita que seus clientes agendem no seu
-          tempo livre.
-        </Text>
+    <>
+      <NextSeo
+        title="Agendamento descomplicado"
+        description="Conecte seu calendário e permita que seus clientes agendem no seu tempo livre."
+      />
 
-        <ClaimUsernameForm />
-      </Hero>
-      <Preview>
-        <Image src={previewImage} alt="" height={400} quality={100} priority />
-      </Preview>
-    </Container>
+      <Container>
+        <Hero>
+          <Heading size="4xl">Agendamento descomplicado</Heading>
+          <Text size="xl">
+            Conecte seu calendário e permita que seus clientes agendem no seu
+            tempo livre.
+          </Text>
+
+          <ClaimUsernameForm />
+        </Hero>
+        <Preview>
+          <Image
+            src={previewImage}
+            alt=""
+            height={400}
+            quality={100}
+            priority
+          />
+        </Preview>
+      </Container>
+    </>
   )
 }
